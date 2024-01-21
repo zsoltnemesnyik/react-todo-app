ae7851ac2033204d43e771ce7d2e38b3074b2ec7
NYKFAN <nemesnyik.zsolt@gmail.com>
2024-01-21 17:17:04 +0100

    update PageTitle component: add other attributes, and format

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,11 +1,13 @@
-import style from '../styles/modules/title.module.scss';
+import style from "../styles/modules/title.module.scss";
 
-const PageTitle = ({children}) => {
+const PageTitle = ({ children, ...rest }) => {
   return (
     <div>
-        <p className={style.title}>{children}</p>
+      <p className={style.title} {...rest}>
+        {children}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default PageTitle
+export default PageTitle;
