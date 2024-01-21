483ddaaac3f8a796d369af6be135226fe2c50bab
NYKFAN <nemesnyik.zsolt@gmail.com>
2024-01-21 18:21:34 +0100

    add AppHeader component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import PageTitle from './components/PageTitle';
+import React from "react";
+import PageTitle from "./components/PageTitle";
+import style from "./styles/modules/app.module.scss";
+import AppHeader from "./components/AppHeader";
 
 function App() {
   return (
     <div className="container">
-      <PageTitle>
-        TODO LIST
-      </PageTitle>
+      <PageTitle>TODO LIST</PageTitle>
+      <div className={style.app__wrapper}>
+        <AppHeader />
+      </div>
     </div>
   );
 }
---
+++ src/components/AppHeader.js
@@ -0,0 +1,9 @@
+const AppHeader = () => {
+  return (
+    <div>
+        <h1>hello from header</h1>
+    </div>
+  )
+}
+
+export default AppHeader
