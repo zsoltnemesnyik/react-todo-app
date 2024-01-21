b405a0f2afe2bd3d3b78931f20df7c543c49fbad
NYKFAN <nemesnyik.zsolt@gmail.com>
2024-01-21 18:22:57 +0100

    create Button component with variants and getClasses function to add classes

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,7 +1,12 @@
+import Button from "./Button"
+
 const AppHeader = () => {
   return (
     <div>
         <h1>hello from header</h1>
+        <Button variant="secondary">
+            click me
+        </Button>
     </div>
   )
 }
